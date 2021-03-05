e093be2bc1f3b6d3c1f5b53a3e964f5780abcc32
Benjamin <ben430hoarau@gmail.com>
2021-03-05 23:02:46 +0400

    Premier commit

---
+++ View/login/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>FirstReseauSocial</title>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <fieldset>
+        <legend>index PHP</legend>
+    </fieldset>
+</body>
+
+</html>
